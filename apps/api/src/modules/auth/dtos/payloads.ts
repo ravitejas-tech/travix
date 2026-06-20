1e275abd3d5730d91657f4a738e2a62d4a285c52
ravitejas <ravitejas@consultbop.com>
2026-06-20 19:53:01 +0530

    feat(api): add DTOs for user registration, login, and refresh token

---
+++ apps/api/src/modules/auth/dtos/payloads.ts
@@ -0,0 +1,18 @@
+import { Type } from '@sinclair/typebox'
+import { Nullable } from '@travix/crud'
+
+export const RegisterPayload = Type.Object({
+    firstName: Nullable(Type.String({ example: 'John' })),
+    lastName: Nullable(Type.String({ example: 'Doe' })),
+    email: Type.String({ format: 'email', example: 'johndoe@example.com' }),
+    password: Type.String({ minLength: 8, maxLength: 64, example: 'abcd@123456' }),
+})
+
+export const LoginPayload = Type.Object({
+    email: Type.String({ format: 'email', example: 'johndoe@example.com' }),
+    password: Type.String({ minLength: 8, maxLength: 64, example: 'abcd@123456' }),
+})
+
+export const RefreshTokenPayload = Type.Object({
+    refreshToken: Type.String(),
+})
