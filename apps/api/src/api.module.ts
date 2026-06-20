d0168247a60686268fd8c971cdba1d04b29265ce
ravitejas <ravitejas@consultbop.com>
2026-06-20 19:55:16 +0530

    feat(api): integrate JWT and Passport modules for authentication

--- apps/api/src/api.module.ts
+++ apps/api/src/api.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { CqrsModule } from '@nestjs/cqrs'
+import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import * as entities from '@travix/db'
 import { LoggerModule } from 'nestjs-pino'
 import { databaseConfig } from './config/database.config'
 import { loggerConfig } from './config/logger.config'
+import { AuthModule } from './modules/auth/auth.module'
 
 @Module({
     imports: [
@@ -32,7 +35,22 @@ import { loggerConfig } from './config/logger.config'
                 }
             },
         }),
+        JwtModule.registerAsync({
+            global: true,
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory(configService: ConfigService) {
+                return {
+                    secret: configService.getOrThrow('JWT_ACCESS_SECRET'),
+                    signOptions: {
+                        expiresIn: configService.get('JWT_ACCESS_EXPIRES_IN', '15m'),
+                    },
+                }
+            },
+        }),
+        PassportModule.register({ defaultStrategy: 'jwt' }),
         CqrsModule.forRoot(),
+        AuthModule,
     ],
 })
 export class ApiModule {}
