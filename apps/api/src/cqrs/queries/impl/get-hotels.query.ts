fe4017d7a2ea6f47737e66ac4a900c6e3950f79b
ravitejas <ravitejas@consultbop.com>
2026-06-20 21:26:11 +0530

    feat(api): add GetHotelsQuery for retrieving hotel data by user and trip ID

---
+++ apps/api/src/cqrs/queries/impl/get-hotels.query.ts
@@ -0,0 +1,8 @@
+import { Query } from '@nestjs/cqrs'
+import { Static } from '@sinclair/typebox'
+import { HotelResponse } from 'api/modules/hotels/dtos/responses'
+
+export class GetHotelsQuery extends Query<Static<typeof HotelResponse>[]> {
+    public readonly userId: string
+    public readonly tripId: string
+}
