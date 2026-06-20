2d6b03f81d6e9cd61ac0727fa590ac5339b2fa25
ravitejas <ravitejas@consultbop.com>
2026-06-20 19:52:01 +0530

    feat(libs): enhance error handling by setting a custom error function in configureNestJsTypebox

--- libs/crud/src/configure.ts
+++ libs/crud/src/configure.ts
@@ -1,11 +1,16 @@
+import { DefaultErrorFunction, SetErrorFunction } from '@sinclair/typebox/errors'
+
 import { setFormats } from './formats'
 import { patchNestJsSwagger } from './swagger-patch'
 import { Configure } from './types'
 
 export const configureNestJsTypebox = (options?: Configure) => {
+    SetErrorFunction((params) => (params.schema as { errorMessage?: string }).errorMessage ?? DefaultErrorFunction(params))
+
     if (options?.patchSwagger) {
         patchNestJsSwagger()
     }
+
     if (options?.setFormats) {
         setFormats()
     }
