7380b44fcb9d2934d851266755db7e8fd800dc18
ravitejas <ravitejas@consultbop.com>
2026-06-20 19:53:50 +0530

    feat(api): create RegisterUserCommand for user registration

---
+++ apps/api/src/cqrs/commands/impl/register-user.command.ts
@@ -0,0 +1,8 @@
+import { Command } from '@nestjs/cqrs'
+import { Static } from '@sinclair/typebox'
+import { RegisterPayload } from 'api/modules/auth/dtos/payloads'
+import { AuthBaseResponse } from 'api/modules/auth/dtos/responses'
+
+export class RegisterUserCommand extends Command<Static<typeof AuthBaseResponse>> {
+    public readonly payload: Static<typeof RegisterPayload>
+}
