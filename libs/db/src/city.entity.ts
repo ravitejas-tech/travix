7087b1169b69cdf16044e433e68020e86d20b05a
ravitejas <ravitejas@consultbop.com>
2026-06-20 18:25:47 +0530

    feat(db): add CityEntity for managing cities in the database

---
+++ libs/db/src/city.entity.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, Index, JoinColumn, ManyToOne } from 'typeorm'
+import { BaseEntity } from './base.entity'
+import { CountryEntity } from './country.entity'
+import { StateEntity } from './state.entity'
+
+@Entity('cities')
+export class CityEntity extends BaseEntity {
+    // properties
+    @Column({ type: 'varchar' })
+    name: string
+
+    // relations
+    @Index()
+    @Column({ type: 'varchar', length: 26, nullable: true })
+    stateId: string | null
+
+    @ManyToOne(() => StateEntity, { nullable: true })
+    @JoinColumn({ name: 'stateId' })
+    state: StateEntity | null
+
+    @Index()
+    @Column({ type: 'varchar', length: 26 })
+    countryId: string
+
+    @ManyToOne(() => CountryEntity)
+    @JoinColumn({ name: 'countryId' })
+    country: CountryEntity
+}
