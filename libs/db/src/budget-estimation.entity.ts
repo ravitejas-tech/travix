abd06f02d7ffa9a4ec058e3b7e516cfe94de5f63
ravitejas <ravitejas@consultbop.com>
2026-06-20 18:25:41 +0530

    feat(db): add BudgetEstimationEntity for managing budget estimations in the database

---
+++ libs/db/src/budget-estimation.entity.ts
@@ -0,0 +1,39 @@
+import { Column, Entity, Index, JoinColumn, ManyToOne, OneToOne } from 'typeorm'
+import { BaseEntity } from './base.entity'
+import { CurrencyEntity } from './currency.entity'
+import { TripEntity } from './trip.entity'
+
+@Entity('budget_estimations')
+export class BudgetEstimationEntity extends BaseEntity {
+    // properties
+    @Column({ type: 'decimal', precision: 10, scale: 2, nullable: true })
+    flights: number | null
+
+    @Column({ type: 'decimal', precision: 10, scale: 2, nullable: true })
+    accommodation: number | null
+
+    @Column({ type: 'decimal', precision: 10, scale: 2, nullable: true })
+    food: number | null
+
+    @Column({ type: 'decimal', precision: 10, scale: 2, nullable: true })
+    activities: number | null
+
+    @Column({ type: 'decimal', precision: 10, scale: 2 })
+    total: number
+
+    // relations
+    @Column({ type: 'varchar', length: 26, unique: true })
+    tripId: string
+
+    @OneToOne(() => TripEntity)
+    @JoinColumn({ name: 'tripId' })
+    trip: TripEntity
+
+    @Index()
+    @Column({ type: 'varchar', length: 26 })
+    currencyId: string
+
+    @ManyToOne(() => CurrencyEntity)
+    @JoinColumn({ name: 'currencyId' })
+    currency: CurrencyEntity
+}
