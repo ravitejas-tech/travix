2a390403b5c9d8bf559b25a31a3b7947d9dd909a
ravitejas <ravitejas@consultbop.com>
2026-06-20 19:53:40 +0530

    feat(api): create GetProfileQuery for retrieving user profile data

---
+++ apps/api/src/cqrs/queries/impl/get-profile.query.ts
@@ -0,0 +1,7 @@
+import { Query } from '@nestjs/cqrs'
+import { Static } from '@sinclair/typebox'
+import { AuthUserResponse } from 'api/modules/auth/dtos/responses'
+
+export class GetProfileQuery extends Query<Static<typeof AuthUserResponse>> {
+    public readonly userId: string
+}
