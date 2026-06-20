aeb51f895bc665b532e1bf4bf032ab077f80499b
ravitejas <ravitejas@consultbop.com>
2026-06-20 21:24:37 +0530

    feat(api): create ItineraryModule with CQRS setup and V1ItineraryController

---
+++ apps/api/src/modules/itinerary/itinerary.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common'
+import { CqrsModule } from '@nestjs/cqrs'
+import { AddActivityHandler } from 'api/cqrs/commands/handlers/add-activity.handler'
+import { RegenerateDayHandler } from 'api/cqrs/commands/handlers/regenerate-day.handler'
+import { RemoveActivityHandler } from 'api/cqrs/commands/handlers/remove-activity.handler'
+import { V1ItineraryController } from './controllers/v1/itinerary.controller'
+
+const CommandHandlers = [RegenerateDayHandler, AddActivityHandler, RemoveActivityHandler]
+
+@Module({
+    imports: [CqrsModule],
+    controllers: [V1ItineraryController],
+    providers: [...CommandHandlers],
+})
+export class ItineraryModule {}
