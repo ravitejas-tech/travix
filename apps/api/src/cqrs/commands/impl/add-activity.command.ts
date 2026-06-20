8729a0cc287ebbee1d0d5e3966675125843f5a5e
ravitejas <ravitejas@consultbop.com>
2026-06-20 21:27:32 +0530

    feat(api): add AddActivityCommand for handling activity addition requests

---
+++ apps/api/src/cqrs/commands/impl/add-activity.command.ts
@@ -0,0 +1,11 @@
+import { Command } from '@nestjs/cqrs'
+import { Static } from '@sinclair/typebox'
+import { AddActivityPayload } from 'api/modules/itinerary/dtos/payloads'
+import { ActivityResponse } from 'api/modules/itinerary/dtos/responses'
+
+export class AddActivityCommand extends Command<Static<typeof ActivityResponse>> {
+    public readonly userId: string
+    public readonly tripId: string
+    public readonly dayId: string
+    public readonly payload: Static<typeof AddActivityPayload>
+}
