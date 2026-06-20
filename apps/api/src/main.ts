86aaaefbd2407fb88d9ccf11d35729b2f515a8eb
ravitejas <ravitejas@consultbop.com>
2026-06-20 17:21:18 +0530

    feat(api): integrate logger into bootstrap process for improved logging

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,6 +5,7 @@ import { NestExpressApplication } from '@nestjs/platform-express'
 import { configureNestJsTypebox } from '@travix/crud'
 import { json, urlencoded } from 'express'
 import helmet from 'helmet'
+import { Logger } from 'nestjs-pino'
 import { ApiModule } from './api.module'
 import { setupSwagger } from './utils/setup-swagger'
 
@@ -14,8 +15,10 @@ configureNestJsTypebox({
 })
 
 async function bootstrap() {
-    const app = await NestFactory.create<NestExpressApplication>(ApiModule)
+    const app = await NestFactory.create<NestExpressApplication>(ApiModule, { bufferLogs: true })
     const configService = app.get(ConfigService)
+    const logger = app.get(Logger)
+    app.useLogger(logger)
 
     app.enableCors({
         origin: '*',
@@ -35,8 +38,8 @@ async function bootstrap() {
     const HOST = configService.get('HOST', '0.0.0.0')
     const PORT = +configService.get('PORT', '3000')
     await app.listen(PORT, HOST, () => {
-        console.log(`API listening on http://${HOST}:${PORT}`)
-        console.log(`Swagger: http://${HOST}:${PORT}/openapi`)
+        logger.log(`API listening on http://${HOST}:${PORT}`)
+        logger.log(`Swagger: http://${HOST}:${PORT}/openapi`)
     })
 }
 
