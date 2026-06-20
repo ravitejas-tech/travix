c5bff6169c9e0eed7fa9c380774cc3c0e274b1c2
ravitejas <ravitejas@consultbop.com>
2026-06-20 21:27:28 +0530

    feat(api): add CreateTripCommand for creating trip requests

---
+++ apps/api/src/cqrs/commands/impl/create-trip.command.ts
@@ -0,0 +1,9 @@
+import { Command } from '@nestjs/cqrs'
+import { Static } from '@sinclair/typebox'
+import { CreateTripPayload } from 'api/modules/trips/dtos/payloads'
+import { TripDetailResponse } from 'api/modules/trips/dtos/responses'
+
+export class CreateTripCommand extends Command<Static<typeof TripDetailResponse>> {
+    public readonly userId: string
+    public readonly payload: Static<typeof CreateTripPayload>
+}
