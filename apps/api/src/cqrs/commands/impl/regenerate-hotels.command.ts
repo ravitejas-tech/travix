7a96c7d11b2a66cb4393ebf1d5233f8a3b99cdac
ravitejas <ravitejas@consultbop.com>
2026-06-20 21:27:15 +0530

    feat(api): add RegenerateHotelsCommand for hotel regeneration requests

---
+++ apps/api/src/cqrs/commands/impl/regenerate-hotels.command.ts
@@ -0,0 +1,8 @@
+import { Command } from '@nestjs/cqrs'
+import { Static } from '@sinclair/typebox'
+import { HotelResponse } from 'api/modules/hotels/dtos/responses'
+
+export class RegenerateHotelsCommand extends Command<Static<typeof HotelResponse>[]> {
+    public readonly userId: string
+    public readonly tripId: string
+}
