405c89936f166421ea2b82ca34f803ad90515382
ravitejas <ravitejas@consultbop.com>
2026-06-20 21:27:08 +0530

    feat(api): add RemoveActivityCommand for handling activity removal requests

---
+++ apps/api/src/cqrs/commands/impl/remove-activity.command.ts
@@ -0,0 +1,8 @@
+import { Command } from '@nestjs/cqrs'
+
+export class RemoveActivityCommand extends Command<void> {
+    public readonly userId: string
+    public readonly tripId: string
+    public readonly dayId: string
+    public readonly activityId: string
+}
