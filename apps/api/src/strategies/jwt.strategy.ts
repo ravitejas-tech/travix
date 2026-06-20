7ec3bcaa407a5c162855b8afbfad81f3296315f2
ravitejas <ravitejas@consultbop.com>
2026-06-20 19:52:21 +0530

    feat(api): implement JwtStrategy for JWT authentication

---
+++ apps/api/src/strategies/jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { Inject, Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { PassportStrategy } from '@nestjs/passport'
+import { ExtractJwt, Strategy } from 'passport-jwt'
+
+export interface JwtPayload {
+    id: string
+    email?: string | null
+}
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+    constructor(@Inject(ConfigService) private readonly configService: ConfigService) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: configService.getOrThrow('JWT_ACCESS_SECRET'),
+        })
+    }
+
+    async validate(payload: JwtPayload) {
+        return {
+            id: payload.id,
+            email: payload.email,
+        }
+    }
+}
