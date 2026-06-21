8002f6ab79d790cb65f14b89baf3482a33911e27
ravitejas <ravitejas@consultbop.com>
2026-06-21 15:43:36 +0530

    feat(web): restructure dashboard routes to include layout component

--- apps/web/app/routes.ts
+++ apps/web/app/routes.ts
@@ -1,8 +1,18 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  layout,
+  route,
+} from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
   route("login", "routes/auth/login.tsx"),
   route("register", "routes/auth/register.tsx"),
-  route("dashboard", "routes/dashboard.tsx"),
+  layout("routes/dashboard/layout.tsx", [
+    route("dashboard", "routes/dashboard/home.tsx"),
+    route("dashboard/trips", "routes/dashboard/trips.tsx"),
+    route("dashboard/trips/:tripId", "routes/dashboard/trip.tsx"),
+  ]),
 ] satisfies RouteConfig;
+
