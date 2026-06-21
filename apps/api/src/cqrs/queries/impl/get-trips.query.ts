ae60ad16cf16847b04d23298445904959e1d30cd
ravitejas <ravitejas@consultbop.com>
2026-06-21 17:07:02 +0530

    feat(api): add search and sorting parameters to GetTripsQuery

--- apps/api/src/cqrs/queries/impl/get-trips.query.ts
+++ apps/api/src/cqrs/queries/impl/get-trips.query.ts
@@ -1,6 +1,7 @@
 import { Query } from '@nestjs/cqrs';
 import { Static } from '@sinclair/typebox';
 import { PaginatedTripsResponse } from 'api/modules/trips/dtos/responses';
+import { TripSort } from 'api/modules/trips/dtos/payloads';
 
 export class GetTripsQuery extends Query<
   Static<typeof PaginatedTripsResponse>
@@ -8,4 +9,6 @@ export class GetTripsQuery extends Query<
   public readonly userId: string;
   public readonly page: number;
   public readonly limit: number;
+  public readonly search?: string;
+  public readonly sort?: TripSort;
 }
